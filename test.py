daf27c41fb982d1122721d379c61ca2bbf930533
anidisc <aniello@discala.org>
2025-08-15 22:20:38 +0200

    feat: Implement cut selection functionality and refactor code

---
+++ test.py
@@ -0,0 +1,34 @@
+import math
+
+def miafunzione:
+#questo e un commento
+
+
+x=1
+while (x==10):
+    print(f"dire che va bene")
+    x++
+    
+
+sappiamo
+
+x=1
+while (x==10):
+    print(f"dire che va bene")
+        x++
+        
+x=1while (x==10):
+    print(f"dire che va bene")
+        x++
+        x=1
+while (x==10):
+    print(f"dire che va bene")
+        x++
+                   
+        
+while (x==10):    print(f"dire che va bene")
+        x++
+        
+x=1
+while (x==10):                  
+        
